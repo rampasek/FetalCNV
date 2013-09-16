5ac27caced03819fb9473748fb32e3ba238d8995
Aryan <aryan.arbabi@gmail.com>
2013-09-16 19:09:04 -0400

    duplicator

--- fcnv/Aryan/maternal_duplicate.py
+++ fcnv/Aryan/maternal_duplicate.py
@@ -3,10 +3,7 @@ import argparse
 import random
 
 ALL_PROPERLY_ALIGNED = 0x2
-IM1=9
-IP1=10
-IC1=11
-
+sourceIndex={'IM1':9,'IP1':10,'IC1':11}
 
 def mapping_parser(m):
     '''
@@ -71,39 +68,46 @@ def main():
 
     reads_file=open(args.filenames[0], "r")
     snips_file=open(args.filenames[1], "r")
-    goalHaplotype=args.filenames[2]
+    source=args.filenames[2]
+    goalHaplotype=args.filenames[3]
 
 
     snips=[]
 
     nsnips=0
 
-    # copy snips from file to RAM
+    # Copy snips from file to RAM
     for line in snips_file:
         if line[0]=='#':
             continue
         if isinstance(line, str):
             line = line.strip().split('\t')
             haplos=[line[3], line[4]]
-            phasing=line[IM1].strip().split(':')[0].strip().split('|');
+            phasing=line[9].strip().split(':')[0].strip().split('|')
             if (phasing[0] != phasing[1]):
                 snips.append({"pos":int(line[1]),
                               "chr":('chr'+line[0]),
                               "HA": (haplos[int(phasing[0])]),
                               "HB": (haplos[int(phasing[1])])
                              })
+    # Use this if the previous format for the phasing file is being used
 #            line = line.strip().split('\t')
 #            snips.append({"pos":int(line[2].strip().split(':')[1]),
 #                          "chr":(line[2].strip().split(':')[0]),
 #                          "HA": (line[5][0]),
 #                          "HB": (line[6][0])
 #                         })
-#    print upper_bound(snips, 'chr20', 10181440, 0, len(snips));
-#    print upper_bound(snips, 'chr20', 10281440, 0, len(snips));
-#    exit()
-    #for each read:
+
+    # Number of Snips in the region:
+#   print upper_bound(snips, 'chr20', 10181440, 0, len(snips));
+#   print upper_bound(snips, 'chr20', 10281440, 0, len(snips));
+#   exit()
+
+    # For each read count the snips in the read and check if it belongs to the target hapoltype:
     for read in reads_file:
         parsed_read= mapping_parser(read)
+
+        # If the read is not aligned properly, ignore it
         if parsed_read['flag'] & ALL_PROPERLY_ALIGNED == 0: continue 
         if parsed_read['mapq'] < 20: continue 
 
@@ -111,11 +115,14 @@ def main():
         pos_db = parsed_read['pos']
         op = parse_cigar_string(parsed_read['cigar'])
 
+        # Search for the start of the related snips
         snip = upper_bound(snips, parsed_read['chr'], parsed_read['pos'], 0, len(snips));
+
         countA=0
         countB=0
         countMis=0
 
+        # Count the snips for each haplotype using the cigar string
         for o in op:
             while pos_db>snips[snip]['pos'] and snip<len(snips) and snips[snip]['chr']==parsed_read['chr']:
                 snip += 1
@@ -127,7 +134,6 @@ def main():
             elif o[0] in 'ND': pos_db += o[1]
             elif o[0] in 'M=X':
                 for i in range(o[1]):
-                    #if the read position is of sufficient quality, record this info
                     if pos_db==snips[snip]['pos'] and ord(parsed_read['qual'][pos_qr]) >= 33+20:
                         if snips[snip]['HA']==parsed_read['seq'][pos_qr]:
                             countA += 1
@@ -138,12 +144,13 @@ def main():
                     pos_db += 1
                     pos_qr += 1
 
+        # Find the haplotype for the read 
         if countA>countB and goalHaplotype=='A':
             print read,
         if countA<countB and goalHaplotype=='B':
             print read,
-#        if countA==countB and random.randrange(0,2)==1:
-#            print read,
+        if countA==countB and random.randrange(0,2)==1:
+            print read,
 
 
 if __name__ == '__main__':
--- fcnv/Aryan/sim_maternal_duplicate.sh
+++ fcnv/Aryan/sim_maternal_duplicate.sh
@@ -3,12 +3,22 @@ region=chr20:10181440-10281440
 #phase_sites=~/fetusProject/I1/chr20/i1_phase_blocks_and_sites.txt 
 phase_sites=./trio.phase.vcf
 haplotype=B
+source=IM1
+plasmaFetusRate=0.13
+plasmaCoverage=78
+readLength=100
 
-echo "copying all the reads in the region..."
-samtools view $bamfile $region > tmp_reads
+echo "Copying all the reads in the region..."
+samtools view $bamfile $region > raw_reads
 echo "done."
 
-echo "filtering for the correct haplotype"
-python maternal_duplicate.py tmp_reads $phase_sites $haplotype > resultsB 
+echo "Filtering for the correct haplotype..."
+python maternal_duplicate.py raw_reads $phase_sites $source $haplotype > filteredResults
 echo "done."
+
+echo "Down sampling the results..."
+numReads=`wc -l filteredResults | awk '{print $1}'`
+python maternal_duplicate_down_sampler.py resultsA $plasmaFetusRate $plasmaCoverage $readLength $numReads $region > $source-$haplotype-$region-duplicate.sam
+echo "done."
+
 
