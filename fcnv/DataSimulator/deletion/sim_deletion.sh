05cf243f7f6e9668e85452126608eff1f2402ded
Ladislav Rampasek <rampasek@gmail.com>
2013-09-18 22:48:46 -0400

    temp file renaming

--- fcnv/DataSimulator/deletion/sim_deletion.sh
+++ fcnv/DataSimulator/deletion/sim_deletion.sh
@@ -1,29 +1,43 @@
-phase_sites=./trio.phase.vcf
-bamfile=~/fetusProject/I1/chr20/I1_M_chr20.bam
+#!/bin/bash
+
+phase_sites=../../chr20/trio.phase.vcf
+bamfile=../../chr20/__plasma.part.bam
 readLength=100
 plasmaFetusRate=0.13
 
 chromosome=chr20
 begin=10181440
-end=10281440
+end=20181440
 haplotype=B
 
 regionCompliment=$chromosome':1-'$((begin-readLength))' '$chromosome':'$((end+readLength))
 region=$chromosome':'$begin'-'$end
 
+logfile=log_simDel.$$.log
+exec > $logfile 2>&1
+
+echo "$region $haplotype $source"
+
 echo "Copying all the reads in the region..."
-samtools view $bamfile $region > raw_reads
+samtools view $bamfile $region > raw_reads$$    
 # Copying the header
-samtools view -H $bamfile $region > inside.sam
+samtools view -H $bamfile $region > inside$$.sam
 
 echo "Filtering the reads that are not deleted..."
-python deletion.py raw_reads $phase_sites $plasmaFetusRate $haplotype >> inside.sam
-samtools view -S -b inside.sam > inside.bam 
+python deletion.py raw_reads$$ $phase_sites $plasmaFetusRate $haplotype >> inside$$.sam
+samtools view -S -b inside$$.sam > inside$$.bam 
 
 echo "Adding reads located out of the region..."
-samtools view -h -b $bamfile $regionCompliment > outside.bam
+samtools view -h -b $bamfile $regionCompliment > outside$$.bam
 
 echo "Merging"
 # Outside has the headers
-samtools merge -f $haplotype-$region-delete.bam outside.bam inside.bam
+samtools merge -f $haplotype-$region-delete.bam outside$$.bam inside$$.bam
+echo "Sorting"
+samtools sort $haplotype-$region-delete.bam $haplotype-$region-delete.bam.sort
+echo "Indexing"
+samtools index $haplotype-$region-delete.bam.sort.bam
+
+rm $haplotype-$region-delete.bam
+rm outside$$.bam inside$$.bam raw_reads$$
 
--- fcnv/DataSimulator/duplication/sim_duplicate.sh
+++ fcnv/DataSimulator/duplication/sim_duplicate.sh
@@ -1,6 +1,8 @@
-phase_sites=./trio.phase.vcf
-bamfile=~/fetusProject/I1/chr20/I1_M_chr20.bam
-plasmaFile=
+#!/bin/bash
+
+phase_sites=../../chr20/trio.phase.vcf
+bamfile=../../chr20/__P.part.bam
+plasmaFile=../../chr20/__plasma.part.bam
 readLength=100
 plasmaFetusRate=0.13
 plasmaCoverage=78
@@ -9,27 +11,35 @@ chromosome=chr20
 begin=10181440
 end=10281440
 haplotype=B
-source=IM1
+source=IP1
 
 region=$chromosome':'$begin'-'$end
 
+logfile=log_simDup.$$.log
+exec > $logfile 2>&1
+
+echo "$region $haplotype $source" 
+
 echo "Copying all the reads in the region..."
-samtools view $bamfile $region > raw_reads
+samtools view $bamfile $region > raw_reads$$
 # Copying the header
-samtools view -H $bamfile $region > inside.sam
+samtools view -H $bamfile $region > inside$$.sam
 
 echo "Filtering for the correct haplotype..."
-python duplication.py raw_reads $phase_sites $source $haplotype > filteredResults
+python duplication.py raw_reads$$ $phase_sites $source $haplotype > filteredResults$$
 
 echo "Down sampling the results..."
-numReads=`wc -l filteredResults | awk '{print $1}'`
-python duplication_down_sampler.py filteredResults $plasmaFetusRate $plasmaCoverage $readLength $numReads $region >> inside.sam
-samtools view -S -b inside.sam > inside.bam 
+numReads=`wc -l filteredResults$$ | awk '{print $1}'`
+python duplication_down_sampler.py filteredResults$$ $plasmaFetusRate $plasmaCoverage $readLength $numReads $region >> inside$$.sam
+samtools view -S -b inside$$.sam > inside$$.bam 
 
 echo "Merging"
 # Outside has the headers
-samtools merge -f $source-$haplotype-$region-duplicate.bam $plasmaFile inside.bam
+samtools merge -f $source-$haplotype-$region-duplicate.bam $plasmaFile inside$$.bam
+echo "Sorting"
 samtools sort $source-$haplotype-$region-duplicate.bam $source-$haplotype-$region-duplicate.sort
+echo "Indexing"
 samtools index $source-$haplotype-$region-duplicate.sort.bam
 
 rm $source-$haplotype-$region-duplicate.bam
+rm raw_reads$$ inside$$.sam filteredResults$$ inside$$.bam 
