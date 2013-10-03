3f512864d508d4e6fcccac90e7b8b2b7ff9e6919
Ladislav Rampasek <rampasek@gmail.com>
2013-10-03 15:51:12 -0400

    touch up

--- fcnv/span_samtools.sh
+++ fcnv/span_samtools.sh
@@ -14,7 +14,7 @@ do
     reads_file_var=reads$num
     reads_file=${!reads_file_var}
     #echo "samtools mpileup -f $ref -uDSI -Q10 -q10 -l $pos $reads_file | bcftools view -g - > $prefix.$num.vcf"
-    samtools mpileup -f $ref -uDSI -Q20 -q10 -l $pos $reads_file | bcftools view -g - > $prefix.$num.vcf &
+    samtools mpileup -f $ref -uDSI -Q15 -q10 -l $pos $reads_file | bcftools view -g - > $prefix.$num.vcf &
 done
 
 #pileup plasma and maternal reads to get DOC for all positions
@@ -22,7 +22,7 @@ for num in 1 2
 do
     reads_file_var=reads$num
     reads_file=${!reads_file_var}
-    samtools mpileup -Q20 -q10 -l $valid_regions $reads_file | awk '{print $2,$4}' > $pile_prefix.$num.txt &
+    samtools mpileup -Q15 -q10 -l $valid_regions $reads_file | awk '{print $2,$4}' > $pile_prefix.$num.txt &
 done
 
 wait
