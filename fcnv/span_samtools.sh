38f2b815e80e10f24cb0387cafb3fae154fdcefc
Ladislav Rampasek <rampasek@gmail.com>
2013-10-15 22:51:49 -0400

    touch ups

--- fcnv/span_samtools.sh
+++ fcnv/span_samtools.sh
@@ -11,25 +11,30 @@ wend=$8
 chr=$9
 pile_prefix=${10}
 
+cp /dupa-filer/laci/I1/$chr/$chr.fa $tmp_prefix.ref.fa
+
 for num in 1 2 3
 do
     reads_file_var=reads$num
     reads_file=${!reads_file_var}
     #echo "samtools mpileup -f $ref -uDSI -Q10 -q10 -l $pos $reads_file | bcftools view -g - > $prefix.$num.vcf"
-    samtools mpileup -f $ref -uDSI -Q10 -q10 -l $pos $reads_file | bcftools view -g - > $tmp_prefix.$num.vcf &
+    samtools mpileup -f $tmp_prefix.ref.fa -uDSI -Q10 -q10 -l $pos $reads_file | bcftools view -g - > $tmp_prefix.$num.vcf &
 done
 
 nums='1' #'1 2'
 #pileup plasma and maternal reads to get DOC for all positions
 for num in $nums
 do
-    echo "pileup plasma: $chr:$wbegin-$wend"
+    echo "pileup plasma: $chr:$wbegin-$wend $pile_prefix.$num.txt"
     reads_file_var=reads$num
     reads_file=${!reads_file_var}
     samtools mpileup -Q10 -q10 -r $chr:$wbegin-$wend $reads_file | awk '{print $2,$4}' > $pile_prefix.$num.txt & #$tmp_prefix.$num.txt
+    #echo "samtools mpileup -Q10 -q10 -l $wbegin $reads_file | awk "'{print $2,$4}'" > $pile_prefix.$num.txt"
+    #samtools mpileup -Q10 -q10 -l $wbegin $reads_file | awk '{print $2,$4}' > $pile_prefix.$num.txt & #$tmp_prefix.$num.txt
 done
 wait
 
+rm $tmp_prefix.ref.fa
 
 #move the pileup files to their destination
 #for num in $nums
